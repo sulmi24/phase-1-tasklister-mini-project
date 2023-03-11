2e8c409a5f2a106be4deb8805ed937bd4aee3dd4
sulmi24 <a.sulmi24@gmail.com>
2023-03-11 22:02:32 +0200

    completed assignemnt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleForm(e.target.new_task_description.value);
+  });
 });
+
+function handleForm(data) {
+  let app = document.querySelector("#list > #tasks");
+
+  let newItem = document.createElement("li");
+
+  app.appendChild(newItem);
+  newItem.textContent = data;
+  let newItemDelete = document.createElement("button");
+
+  newItem.appendChild(newItemDelete);
+
+  newItemDelete.textContent = "x";
+
+  newItemDelete.addEventListener("click", (e) => {
+    e.target.parentNode.remove();
+  });
+}
